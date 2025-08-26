784be785f49e72c5927fe96adcdbf4964ae925b9
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-08-26 13:24:53 +0200

    initial commit

--- ovinger/oving2/leapYear.java
+++ ovinger/oving2/leapYear.java
@@ -1,10 +1,20 @@
-import java.util.Scanner;
+import java.util.*; 
 
 public class leapYear {
-    public static void main(String[]args){
+    public static void main(String[]args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Type in a year: "); //Printer ut en overskrift og et felt hvor man kan skrive inn antall tommer
+
+        int year = input.nextInt();
         
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            System.out.println(year + " er et skuddår");
+        } else {
+            System.out.println(year + " er ikke et skuddår"); 
+        } 
+
     }
+
 }
