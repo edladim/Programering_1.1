9508ca7ae64af173f698c85cd5b16e3e4a31b4e7
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-09-04 10:43:21 +0200

    updated varibles and made method shorter

--- ovinger/oving3/multiplicationTable.java
+++ ovinger/oving3/multiplicationTable.java
@@ -5,23 +5,22 @@ public class multiplicationTable {
 
         Scanner input = new Scanner(System.in);
         System.out.print("Which part of the multiplicationTable do you want to view?"); 
-        String numbers = input.nextLine();
+        String factors = input.nextLine();
 
-        String[] calculation = numbers.split("[-|.|*|x|,]");
-        int number1 = Integer.parseInt(calculation[0]);
-        int number2 = Integer.parseInt(calculation[1]);
+        String[] calculation = factors.split("[-|.|*|x|,]");
+        int start = Integer.parseInt(calculation[0]);
+        int end = Integer.parseInt(calculation[1]);
 
-        multiplication(number1, number2);
+        multiplication(start, end);
        
         input.close();
     }
 
-    public static void multiplication(int number1, int number2){
-        for (int i = 0; i <= number2 - number1; i++){
-            int number = number1 + i;
-            System.out.println("\n" + number + "-table:");
-            for (int j = 1; j <= 10; j++){
-                System.out.println(number + " * " + j + " = " + number * j);
+    public static void multiplication(int start, int end){
+        for (int factor = start; factor <= end; factor++){
+            System.out.println("\n" + factor + "-table:");
+            for (int multiplier = 1; multiplier <= 10; multiplier++){
+                System.out.println(factor + " * " + multiplier + " = " + factor * multiplier);
             }
         }
     }   
