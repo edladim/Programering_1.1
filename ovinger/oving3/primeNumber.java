21ff919dd25cbc4d0b29215e6b5addf56d7dcd76
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-09-08 19:05:10 +0200

    complited the program

--- ovinger/oving3/primeNumber.java
+++ ovinger/oving3/primeNumber.java
@@ -3,32 +3,34 @@ public class primeNumber {
     public static void main(String[]args){
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Type any number to check if it is a prime number: ");
 
         while(true){
+            System.out.print("Type a number to check if it is a prime number or type 0 to quit the program: ");
             String in = input.nextLine();
             try {
                 int number = Integer.parseInt(in);
                 if (number == 0){
-
+                    System.out.println("Quitting program");
+                    break;
                 }
-                System.out.println("Avslutter programmet");
-                break
-                if 
 
-            } catch (Exception e) {
+                if (isPrime(number)){
+                    System.out.println(number + " is a prime number.");
+                }else{
+                    System.out.println(number + " is not a prime number.");
+                }
+
+            }catch (Exception e) {
                 System.out.println("Please wright an integer");
             }
         }
-
-        public static boolean isPrime(int n){
-            
-            if (n <= 1) return false;
-            
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) return false;
-            }
-            return true;
+        input.close();
+    }
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) return false;
         }
+        return true;
     }
 }
