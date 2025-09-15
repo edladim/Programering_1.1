3f1fbc9f4bae69e4223fac07e04ca797c41436b3
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-09-15 19:05:18 +0200

    finished the player class

--- ovinger/oving4/diceRoll.java
+++ ovinger/oving4/diceRoll.java
@@ -7,7 +7,33 @@ public class diceRoll {
     }
 }
 
-calss Player{
+class Player{
 
-    //Instance field
+    //--- Instance field ---
+    private int sumPoints = 0;
+    java.util.Random dice = new java.util.Random();
+
+    //--- Methods ---
+    public int getSumPoints(){
+        return sumPoints;
+    }
+
+    public boolean isDone(){
+        return sumPoints == 100;
+    }
+    
+    public void rollDice(){
+        int diceRoll = dice.nextInt(6) + 1;
+        
+        if (diceRoll == 1){
+            sumPoints = 0;
+        }
+        else{
+            sumPoints += diceRoll;
+        }
+
+        if(sumPoints > 100){
+            sumPoints -= diceRoll;
+        }
+    }
 }
