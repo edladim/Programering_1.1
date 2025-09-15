98c0317af9e7a619aafb8bab0f27cf5539859390
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-09-15 17:09:27 +0200

    complited the program

--- ovinger/oving4/currencyConvertor.java
+++ ovinger/oving4/currencyConvertor.java
@@ -1,28 +1,79 @@
+import java.util.*; 
+
 public class currencyConvertor{
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\n--- Valutakalkulator ---");
-        System.out.println("Velg valuta:");
-        System.out.println("1: Amerikanske dollar (USD)");
-        System.out.println("2: Euro (EUR)");
-        System.out.println("3: Svenske kroner (SEK)");
-        System.out.println("4: Avslutt");
-        System.out.print("Skriv inn ditt valg: ");
+        Currency dollar = new Currency("USD", 10.5);
+        Currency euro = new Currency("EUR", 11.2);    
+        Currency svenskeKroner = new Currency("SEK", 0.95); 
 
 
+        while (true) {
+            System.out.println("\n--- CurencyConvertor ---");
+            System.out.println("Choose a currency:");
+            System.out.println("1: American dollar (USD)");
+            System.out.println("2: Euro (EUR)");
+            System.out.println("3: Svenske kroner (SEK)");
+            System.out.println("4: End program");
+            System.out.print("Type in your choice: ");
 
+            try {
+                int choice = scanner.nextInt();
+
+                if (choice == 4) {
+                    System.out.println("--- Quitting program ---");
+                    break; // Avslutter programmet
+                }
+
+                Currency selectedCurrency = null;
+                String selectedCurrencyName = "";
+
+           
+                switch (choice) {
+                    case 1:
+                        selectedCurrency = dollar;
+                        selectedCurrencyName = "USD";
+                        break;
+                    case 2:
+                        selectedCurrency = euro;
+                        selectedCurrencyName = "EUR";
+                        break;
+                    case 3:
+                        selectedCurrency = svenskeKroner;
+                        selectedCurrencyName = "SEK";
+                        break;
+                    default:
+                        System.out.println("Ivalid input, please try agian.");
+                        continue;
+                }
+
+                System.out.print("Type in amount of " + selectedCurrencyName + ": ");
+                double amount = scanner.nextDouble();
+
+                
+                double amountInNok = selectedCurrency.currencyToNok(amount);
+                System.out.printf("%.2f %s == %.2f NOK.%n", amount, selectedCurrencyName, amountInNok);
+
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Please type a number from 1 to 4.");
+                scanner.next(); 
+            }
+        }
+        scanner.close();
     }
 }
-class currency {
+
+class Currency {
 
     //Instance field
     private String name;
-    private float rate;
+    private double rate;
 
     //Constructor
-    public currency(String n, float r) {
-        name = n;
-        rate = r;
+    public Currency(String name, double rate) {
+        this.name = name;
+        this.rate = rate;
     }
 
     //Methodes
