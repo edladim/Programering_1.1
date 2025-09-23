8d94d6d78329a0d76d69db318e0967b26f35c15e
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-09-23 12:12:41 +0200

    initial commit

---
+++ ovinger/oving6/TextAnalysis.java
@@ -0,0 +1,14 @@
+class TextAnalysis {
+
+    // Instance field
+    private int[] sumCharacters = new int[30];
+    private String text;
+
+    // Constructor
+    TextAnalysis(String text){
+        for (char character : text) {
+            this.sumCharacters[]
+        }
+        this.sumCharacters = sumCharacters;
+    }
+}
