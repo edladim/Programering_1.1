8e3ee519cac302e9fa9c50556ad023a8b321e1b7
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-10-10 11:21:50 +0200

    Created a person class with javadocs

---
+++ ovinger/oving8/Person.java
@@ -0,0 +1,48 @@
+/**
+ * Represents a person with first name last name and, year of birth.
+ */
+public class Person {
+  private String firstName;
+  private String lastName;
+  private int yearOfBirth;
+
+  /**
+   * Constructs a new person with the specifyed first name, last name and year of birth.
+   *
+   * @param firstName the first name of the person
+   * @param lastName the last name of the person
+   * @param yearOfBirth the year of birth og the person
+   */
+  public Person(String firstName, String lastName, int yearOfBirth) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.yearOfBirth = yearOfBirth;    
+  }
+
+  /**
+   * Returnes the first name of the person.
+   *
+   * @return the first name
+   */
+  public String getFirstName() {
+    return firstName;
+  }
+
+  /**
+   * Returns the last name of the person.
+   *
+   * @return the last name
+   */
+  public String getLastName() {
+    return lastName;
+  }
+
+  /**
+   * Returns the persons year of birth.
+   *
+   * @return the year of bith
+   */
+  public int getYearOfBirth() {
+    return yearOfBirth;
+  }
+}
