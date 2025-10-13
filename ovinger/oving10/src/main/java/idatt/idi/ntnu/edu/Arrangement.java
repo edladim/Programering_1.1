99121180b0d04bc9144b3bf54a17dff3ba9b2583
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-10-13 17:00:41 +0200

    constructed arrangement class

---
+++ ovinger/oving10/src/main/java/idatt/idi/ntnu/edu/Arrangement.java
@@ -0,0 +1,10 @@
+package idatt.idi.ntnu.edu;
+
+public class Arrangement {
+  private int number;
+  private String name;
+  private String place;
+  private String organizer;
+  private String type;
+  private int time;
+}
