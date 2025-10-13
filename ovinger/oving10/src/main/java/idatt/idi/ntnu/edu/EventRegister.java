beb66618d8eb24f408801d64d2b4d9e5211d754c
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-10-13 18:54:22 +0200

    inital commit

---
+++ ovinger/oving10/src/main/java/idatt/idi/ntnu/edu/EventRegister.java
@@ -0,0 +1,17 @@
+package idatt.idi.ntnu.edu;
+
+
+public class EventRegister {
+  private event event;
+
+  public eventRegister(Event event) {
+    this.event = event;
+  }
+
+  public void registerNewevent(Event event) {
+    this.event = event;
+
+  }
+
+  public String findArrangemetAt()
+}
