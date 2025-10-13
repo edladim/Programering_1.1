7966da0f0360d1c51cf623a901a55f4ee744d509
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-10-13 16:46:26 +0200

    initial commit

---
+++ ovinger/oving10/src/main/java/idatt/idi/ntnu/edu/App.java
@@ -0,0 +1,13 @@
+package idatt.idi.ntnu.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
