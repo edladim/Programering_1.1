eabb967bbad435f106aab7641b28ac0c418723cb
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-10-14 11:13:44 +0200

    added get events at location methode

--- ovinger/oving10/src/main/java/idatt/idi/ntnu/edu/EventRegister.java
+++ ovinger/oving10/src/main/java/idatt/idi/ntnu/edu/EventRegister.java
@@ -1,17 +1,33 @@
 package idatt.idi.ntnu.edu;
 
+import java.nio.charset.CoderResult;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+
+/**
+ * A register for managing events.
+ */
 public class EventRegister {
-  private event event;
-
-  public eventRegister(Event event) {
-    this.event = event;
-  }
+  private List<Event> events = new ArrayList<>();
 
   public void registerNewevent(Event event) {
-    this.event = event;
-
+    events.add(event);
   }
 
-  public String findArrangemetAt()
+  
+  /**
+   * Returns a list of all events at the specified location.
+   *
+   * @param location the location to filter events by
+   * @return a list of events at the given location
+   */
+  public List<Event> getAllEventsAt(String location) {
+    return events.stream()
+        .filter((event) -> event.getLocation()
+        .equalsIgnoreCase())
+        .collect(Collectors.toList());
+  }
+
 }
