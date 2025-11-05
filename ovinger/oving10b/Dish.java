c69598e6717c87bbe2b1a678be52766d855169b7
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-11-05 16:09:20 +0100

    Add getters and toString

--- ovinger/oving10b/Dish.java
+++ ovinger/oving10b/Dish.java
@@ -1,6 +1,34 @@
 public class Dish {
-    private final String name;
-    private final DishType type;
-    private final double price;
-    private final String recipe;
+  private final String name;
+  private final DishType type;
+  private final double price;
+  private final String recipe;
+
+  public Dish(String name, DishType type, double price, String recipe) {
+    this.name = name;
+    this.type = type;
+    this.price = price;
+    this.recipe = recipe;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public DishType getType() {
+    return type;
+  }
+
+  public double getPrice() {
+    return price;
+}
+
+  public String getRecipe() {
+    return recipe;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("%s (%s) - %.2f", name, type, price);
+  }
 }
