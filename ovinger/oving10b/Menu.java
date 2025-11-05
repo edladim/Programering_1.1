e9b18f9c83398851dfa0df2b02d21dcb16768da3
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-11-05 16:30:34 +0100

    Add getters and toString

--- ovinger/oving10b/Menu.java
+++ ovinger/oving10b/Menu.java
@@ -1,5 +1,6 @@
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Menu {
   private final String name;
@@ -9,4 +10,19 @@ public class Menu {
     this.name = name;
     this.dishes = new LinkedHashSet<>(dishes);
   }
+  public String getName() {
+      return name;
+    }
+
+    public Set<Dish> getDishes() {
+      return dishes;
+    }
+
+    @Override
+    public String toString() {
+      String dishList = dishes.stream()
+          .map(Dish::toString)
+          .collect(Collectors.joining(", "));
+      return String.format("Menu: %s | Dishes: [%s] | Total: %.2f", name, dishList);
+    }
 }
