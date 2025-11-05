0f43ac59b0901eb95136ed5dc5cae03ec4445e60
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-11-05 16:56:16 +0100

    Implement MenuRegister class with dishes and menus collections

---
+++ ovinger/oving10b/MenuRegister.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+
+
+public class MenuRegister {
+  private final Map<String, Dish> dishes = new LinkedHashMap<>();
+  private final List<Menu> menus = new ArrayList<>();
+  
+}
+
+
