0c0bab1435ce93243b7dd173249b74ef42363726
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-11-05 18:55:28 +0100

    Add findMenusByPriceRange and registerMenu methode

--- ovinger/oving10b/MenuRegister.java
+++ ovinger/oving10b/MenuRegister.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.List;
 
 
@@ -20,6 +23,26 @@ public class MenuRegister {
     return dishes.values().stream()
         .filter(dish -> dish.getType() == type)
         .toList();
+  }
+
+  public void registerMenu(String menuName, Set<String> dishNames) {
+    Set<Dish> menuDishes = new LinkedHashSet<>();
+    for (String name : dishNames) {
+      menuDishes.add(dishes.get(name));
+    }
+    menus.add(new Menu(menuName, menuDishes));
+  }
+
+  public List<Menu> findMenusByPriceRange(double min, double max) {
+    return menus.stream()
+        .filter(menu -> {
+            double price = menu.totalPrice();
+            return price >= min && price <= max;})
+        .toList();
+  }
+
+  public List<Menu> allMenus() {
+    return new ArrayList<>(menus);
   }
 }
 
