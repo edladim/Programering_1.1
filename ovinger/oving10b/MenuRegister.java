7c3096aec0cd6bc0aabde964c15a5709676340e9
Edvard Kvisler Ladim <eddy.ladim@gmail.com>
2025-11-05 17:03:22 +0100

    Add methods to register and find dishes in MenuRegister

--- ovinger/oving10b/MenuRegister.java
+++ ovinger/oving10b/MenuRegister.java
@@ -7,7 +7,20 @@ import java.util.List;
 public class MenuRegister {
   private final Map<String, Dish> dishes = new LinkedHashMap<>();
   private final List<Menu> menus = new ArrayList<>();
-  
+
+  public void registerDish(Dish dish) {
+    dishes.put(dish.getName(), dish);
+  }
+
+  public Dish findDishByName(String name) {
+    return dishes.get(name);
+  }
+
+  public List<Dish> findDishesByType(DishType type) {
+    return dishes.values().stream()
+        .filter(d -> d.getType() == type)
+        .toList();
+  }
 }
 
 
